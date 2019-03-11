c006e2099ac898b030bb492d53ccf3a08fc0ca30
xuruiray <rayxurui@didichuxing.com>
2019-03-11 20:44:13 +0800

    修改 signal

--- main.go
+++ main.go
@@ -16,39 +16,38 @@ func main() {
 		err error
 	)
 
+	fmt.Println("init app")
 	err = config.Init(config.AppConfigFile)
 	if err != nil {
 		fmt.Println(err)
 	}
 
+	fmt.Println("init mysql")
 	err = mysql.Init(config.DataBaseConfigFile)
 	if err != nil {
 		fmt.Println(err)
 	}
 
+	fmt.Println("init redis")
 	err = redis.Init(config.CacheConfigFile)
 	if err != nil {
 		fmt.Println(err)
 	}
 
+	fmt.Println("init http server")
 	err = server.Init(config.MainConfig.Port)
 	if err != nil {
 		fmt.Println(err)
 	}
 
-	initSignal()
 	// wait for system quit
-	<-systemQuit
+	initSignal()
 }
-
-var systemQuit = make(chan int, 1)
 
 //initSignal 初始化信号
 func initSignal() {
 	c := make(chan os.Signal, 1)
 	signal.Notify(c, syscall.SIGINT, syscall.SIGALRM, syscall.SIGTERM, syscall.SIGUSR1)
-
-	// Block until a signal is received.
-	<-c
-	systemQuit <- 1
+	sign := <-c
+	fmt.Println("app terminated by signal", sign)
 }
